bb7611b5002473c449330b7628a913b6ad4b3291
maya-bysell <mayabysell99@hotmail.com>
2026-09-01 15:05:51 +0200

    Update v02_oui.py

--- vecka-02/python/v02_oui.py
+++ vecka-02/python/v02_oui.py
@@ -27,4 +27,4 @@ for address in addresses:
 
     print(f"{address} -> {name}")
 
-<img width="833" height="924" alt="Image" src="https://github.com/user-attachments/assets/c8e262b7-e497-4cf4-abcb-3f4d6d64242d" />
+[image]<img width="833" height="924" alt="Image" src="https://github.com/user-attachments/assets/c8e262b7-e497-4cf4-abcb-3f4d6d64242d" />
